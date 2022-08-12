c720ea79da17886795357ed742d81e89580e53a3
Javier <javier.rostagno@gmail.com>
2022-08-12 09:48:54 -0300

    fix:deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "next-themes";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem attribute="class">
-      <SessionProvider>
+      <SessionProvider session={pageProps.session} refetchInterval={0}>
         <Component {...pageProps} />
       </SessionProvider>
     </ThemeProvider>
